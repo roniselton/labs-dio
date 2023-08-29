9a0412321ffa365214303b92986a0ff775d8068d
Roniselton <ronibarreto@gmail.com>
2023-08-29 00:15:56 -0300

    adicionano elementos no html

--- pokedex/assets/js/pok.js
+++ pokedex/assets/js/pok.js
@@ -2,13 +2,27 @@
 const offset = 10
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-console.log( url );
+//console.log( url );
+
+function convertPokemonToHtml( pokemon ){
+    return `<li> 
+    ${pokemon.name}
+        <img  src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png" >
+    </li>`;
+}
 
 fetch( url )
-.then( (response)=>response.json())
-.then( (jsonBody)=>{
-    debugger
-    console.log( jsonBody )
+.then((response)=>response.json())
+.then((jsonBody)=>jsonBody.results)
+.then((pokemonList)=>{
+    
+    console.log( pokemonList );
+    for(let i in pokemonList){
+        const element = pokemonList[i];
+        let li = convertPokemonToHtml(element);
+        let elem = document.getElementById("OLPokemons");
+        elem.innerHTML +=  li ; 
+    }
 })
 .catch((error)=>console.log( error ))
 .finally(()=>console.log('finally'))
