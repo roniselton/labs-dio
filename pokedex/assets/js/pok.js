2009b5a0a2a2f01ded1a83ec3b5c4fac944ecd15
Roniselton <ronibarreto@gmail.com>
2023-08-28 23:32:13 -0300

    debuger

--- pokedex/assets/js/pok.js
+++ pokedex/assets/js/pok.js
@@ -3,15 +3,12 @@ const offset = 10
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 console.log( url );
-fetch( url ).then(function(response){    
-    return response.json()
+
+fetch( url )
+.then( (response)=>response.json())
+.then( (jsonBody)=>{
+    debugger
+    console.log( jsonBody )
 })
-.then( function( jsonBody ){
-    console.log( responseBody )
-})
-.catch( function(error){
-    console.log('error');
-    console.log( error );
-}).finally(function(){
-    console.log('finally')
-})
+.catch((error)=>console.log( error ))
+.finally(()=>console.log('finally'))
