28f2c407eea12b15c78fe42e0cb51d3dcf6acf1b
Roniselton <ronibarreto@gmail.com>
2023-08-28 23:23:23 -0300

    Chamada requisicao Fetch

--- pokedex/assets/js/pok.js
+++ pokedex/assets/js/pok.js
@@ -1,1 +1,17 @@
-console.log("teste");
+//Fetch API Enxios JQUERY
+const offset = 10
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+console.log( url );
+fetch( url ).then(function(response){    
+    return response.json()
+})
+.then( function( jsonBody ){
+    console.log( responseBody )
+})
+.catch( function(error){
+    console.log('error');
+    console.log( error );
+}).finally(function(){
+    console.log('finally')
+})
